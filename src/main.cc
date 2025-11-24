08ea386df07ac8062bd4439aaed0ea350975816c
Ran Regev <ran.regev@scylladb.com>
2025-11-24 18:46:00 +0200

    better, not perfect

--- src/main.cc
+++ src/main.cc
@@ -60,25 +60,18 @@ class task {
             value_type data_;
     };
 
-    public:
-        task(std::coroutine_handle<task::promise_type> handle) 
-            : this_handle_(handle) {}
-
-        bool await_ready() {return get().has_value();}
-        void await_suspend(std::coroutine_handle<task::promise_type> h) {
-            main_executer.add_coro(h);
-        }
-        value_type await_resume() {
-            return get(); 
-        }
+    private:
+        task(std::coroutine_handle<task::promise_type> handle) : this_handle_(handle) {}
 
     public:
+        void suspend() { main_executer.add_coro(this_handle_); }
         value_type& get() {
             return this_handle_.promise().get();
         }
     private:
         std::coroutine_handle<promise_type> this_handle_;
 };
+
 
 template<>
 class task<void> {
@@ -95,24 +88,57 @@ class task<void> {
             void get() {}
     };
 
-    public:
-        task(std::coroutine_handle<task::promise_type> handle) 
-            : this_handle_(handle) {}
-
-        bool await_ready() {return false;}
-        void await_suspend(std::coroutine_handle<task::promise_type> h) {
-            main_executer.add_coro(h);
-        }
-        void await_resume() {}
+    private:
+        task(std::coroutine_handle<task::promise_type> handle) : this_handle_(handle) {}
 
     public:
+        void suspend() { main_executer.add_coro(this_handle_); }
         void get() {}
+        void set_ready() {
+            ready_ = true;
+        }
+        bool ready() const {
+            return ready_;
+        }
 
     private:
         std::coroutine_handle<promise_type> this_handle_;
+        bool ready_ = true;
 };
 
-task<int> executer_started() {
+template<class T>
+struct awaiter {
+    using value_type = std::optional<T>;
+    awaiter(task<T>& t) : task_(t) {}
+    bool await_ready() {return task_.get().has_value();}
+    void await_suspend(std::coroutine_handle<>) {
+        task_.suspend();
+    }
+    value_type await_resume() {
+        return task_.get(); 
+    }
+
+    task<T> task_;
+};
+
+template<>
+struct awaiter<void> {
+    awaiter(task<void>& t) : task_(t) {}
+    bool await_ready() {return task_.ready();}
+    void await_suspend(std::coroutine_handle<>) {
+        task_.suspend();
+    }
+    void await_resume() {}
+
+    task<void> task_;
+};
+
+template <class T>
+awaiter<T> operator co_await(task<T> t){
+    return awaiter(t);
+}
+
+task<bool> executer_started() {
     co_return main_executer.started();
 }
 
